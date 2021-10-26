f68a824970fc71321ab81777b9b5d9b1ea2e9ea6
Kimmy Liu <kliu7799@gmail.com>
2021-10-25 22:37:09 -0400

    debug

--- parallel_make/parmake.c
+++ parallel_make/parmake.c
@@ -30,76 +30,58 @@ size_t thread_count = 0;
 void quu_push(char* ptr);
 int setup(void* ptr);
 int detect_cycle(void* ptr);
-void* par_run(void* ptr);
+void* part2(void* ptr);
 
 
 
 int parmake(char *makefile, size_t num_threads, char **targets) {
     // good luck!
-
-    q = queue_create(-1);
+    //set up
     d = string_to_int_dictionary_create();
     g = parser_parse_makefile(makefile, targets);
-    if (num_threads < 1) {
-        return 0;
-    }
-
-    vector* neibor = graph_neighbors(g, "");
-    if (vector_empty(neibor)) {
-        rule_t* ru = graph_get_vertex_value(g, "");
-        ru->state = vector_size(neibor);
-        return 0;
-    }
-
-    for (size_t i = 0; i < vector_size(neibor); i++) {
-        char* curr = vector_get(neibor, i);
+    q = queue_create(-1);
+    pthread_t pids[num_threads];
+    vector* goals = graph_neighbors(g, "");
+    //detect cycle
+    for (size_t i = 0; i < vector_size(goals); i++) {
+        char* curr = vector_get(goals, i);
         if (setup((void*)curr) == 1) {
             print_cycle_failure(curr);
-            rule_t* rul = (rule_t*) graph_get_vertex_value(g, (void*)curr);
-            rul->state = -1;
-            vector_erase(neibor, i);
+            rule_t* curr_rule = (rule_t*) graph_get_vertex_value(g, (void*)curr);
+            curr_rule->state = -1;
+            vector_erase(goals, i);
             i--;
         }
     }
 
-    rule_t* ru = graph_get_vertex_value(g, "");
-    ru->state = vector_size(neibor);
-
+    rule_t *root = graph_get_vertex_value(g, "");
+    root->state = vector_size(goals);
+    if (vector_empty(goals)) {return 0;}
+    //push goals to the queue
     int zero = 0;
     vector* vertices = graph_vertices(g);
-    VECTOR_FOR_EACH(vertices, 
-        varname, 
-        {dictionary_set(d, varname, &zero);});
-
+    VECTOR_FOR_EACH(vertices, curr, {dictionary_set(d, curr, &zero);});
     vector_destroy(vertices);
-
-    VECTOR_FOR_EACH(neibor, 
-        varname, 
-        {quu_push(varname);});
-
+    VECTOR_FOR_EACH(goals, vtx, {quu_push(vtx);});
+    //multi thread set up and calculation
     dictionary_destroy(d);
-
-    pthread_t threads[num_threads];
     for (size_t i = 0; i < num_threads; i++) {
-        pthread_create(threads + i, NULL, par_run, NULL);
+        pthread_create(pids + i, NULL, part2, NULL);
     }
-
     pthread_mutex_lock(&mux);
-    while (thread_count != vector_size(neibor)) {
+    while (thread_count != vector_size(goals)) {
         pthread_cond_wait(&cond, &mux);
     }
     pthread_mutex_unlock(&mux);
-    
     for (size_t i = 0; i < num_threads + 1; i++) {
         queue_push(q, NULL);
     }
     for (size_t i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
+        pthread_join(pids[i], NULL);
     }
-
-    queue_destroy(q);
-    vector_destroy(neibor);
+    vector_destroy(goals);
     graph_destroy(g);   
+    queue_destroy(q);
     pthread_cond_destroy(&cond);
     pthread_mutex_destroy(&mux);
     return 0;
@@ -175,53 +157,72 @@ int detect_cycle(void* ptr) {
 
 
 
-void* par_run(void* ptr) {
-    while (1) {
-        char* target = (char*)queue_pull(q);
-        if (!target) {break;}
-        rule_t* rule = (rule_t*)graph_get_vertex_value(g, target);
-        int done = 1;
-        int flag = 0;
-        struct stat stat_inf;
-        if (stat(rule->target, &stat_inf) == -1) {flag = 1;}
-        if (flag == 0) {
+void* part2(void* ptr) {
+    while (true) {
+        char* curr = (char*)queue_pull(q);
+        if (!curr) {
+            break;
+        }
+
+        int flag = 1;
+        int check_stat = 0;
+        struct stat stat_;
+
+        rule_t* val = (rule_t*)graph_get_vertex_value(g, curr);
+        if (stat(val->target, &stat_) == -1) {
+            check_stat = 1;
+        }
+        
+        if (!check_stat) {
             pthread_mutex_lock(&mux);
-            vector* dependencies = graph_neighbors(g, target);
+            vector* neibor = graph_neighbors(g, curr);
             pthread_mutex_unlock(&mux);
-            VECTOR_FOR_EACH(dependencies, vtx, {
-                rule_t* temp_rule = (rule_t*)graph_get_vertex_value(g, vtx);
-                struct stat stat_temp;
-                if (stat(temp_rule->target, &stat_temp) == -1 || stat_temp.st_mtime > stat_inf.st_mtime) {
-                    flag = 1;
-                    break;
-                }
-            });
-            vector_destroy(dependencies);
+            VECTOR_FOR_EACH(neibor, 
+                varname, 
+                {
+                    rule_t* tempr = (rule_t*)graph_get_vertex_value(g, varname);
+                    struct stat stat2;
+                    if (stat(tempr->target, &stat2) == -1 || stat2.st_mtime > stat_.st_mtime) {
+                        check_stat = 1;
+                        break;
+                    }
+                });
+            vector_destroy(neibor);
         }
-        if (flag) {
-            VECTOR_FOR_EACH(rule->commands, vtx, {
-                if (system(vtx) != 0) {
-                    done = 0;
-                    break;
-                }
-            });
+
+        if (check_stat) {
+            VECTOR_FOR_EACH(val->commands, 
+                varname, 
+                {
+                    if (system(varname)) {
+                        flag = 0;
+                        break;
+                    }
+                });
         }
-        //
+
         pthread_mutex_lock(&mux);
-        vector* anti = graph_antineighbors(g, target);
-       VECTOR_FOR_EACH(anti, vtx, {
-            if (done) {
-                rule_t *rule_temp = graph_get_vertex_value(g, vtx);
-                rule_temp->state -= 1;
-                if (rule_temp->state == 0) queue_push(q, vtx);
-            }
-            if (!strcmp(vtx, "")) {
-                thread_count++;
-                pthread_cond_signal(&cond);
-            }
+        vector* anti_neibor = graph_antineighbors(g, curr);
+        VECTOR_FOR_EACH(anti_neibor, 
+            varname, 
+            {
+                if (flag) {
+                    rule_t* val_ = graph_get_vertex_value(g, varname);
+                    val_->state --;
+                    if (!val_->state) {
+                        queue_push(q, varname);
+                    }
+                }
+
+                if (!strcmp(varname, "")) {
+                    thread_count++;
+                    pthread_cond_signal(&cond);
+                }
         });
+
+        vector_destroy(anti_neibor);
         pthread_mutex_unlock(&mux);
-        vector_destroy(anti);
     }
+
     return ptr;
 }
