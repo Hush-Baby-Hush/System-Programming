43e478493ffb21aff37c0d0db2e0864903863e27
Kimmy Liu <kliu7799@gmail.com>
2021-11-15 22:54:09 -0500

    test

--- nonstop_networking/common.c
+++ nonstop_networking/common.c
@@ -24,31 +24,26 @@
 
 ssize_t read_from_socket(int socket, char *buffer, size_t count) {
     // Your Code Here
-    size_t r_count = 0;
-    while (r_count < count) {
-      ssize_t ret = read(socket, buffer + r_count, count - r_count);
-      if (ret == 0) break;
-      if (ret == -1 && errno == EINTR) continue;
-      if (ret == -1) {
-        perror("read_from_socket");
-        return -1;
-      }
-      r_count += ret;
+
+    size_t result = 0;
+    while (result < count) {
+      ssize_t read_ = read(socket, buffer + result, count - result);
+      if (read_ == -1 && errno == EINTR) continue;
+      if (read_ == 0) break;
+      if (read_ == -1) return -1;
+      result += read_;
     }
-    return r_count;
+    return result;
 }
 
 ssize_t write_to_socket(int socket, const char *buffer, size_t count) {
-    size_t w_count = 0;
-    while (w_count < count) {
-      ssize_t ret = write(socket, buffer + w_count, count - w_count);
-      if (ret == 0) break;
-      if (ret == -1 && errno == EINTR) continue;
-      if (ret == -1) {
-        perror("write_from_socket");
-        return -1;
-      }
-      w_count += ret;
+    size_t result = 0;
+    while (result < count) {
+      ssize_t write_ = write(socket, buffer + result, count - result);
+      if (write_ == -1 && errno == EINTR) continue;
+      if (write_ == 0) break;
+      if (write_ == -1) return -1;
+      result += write_;
     }
-    return w_count;
+    return result;
 }
