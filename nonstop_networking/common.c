584ac0da5f073bb52b975cf807f29fbc3e029eb1
Kimmy Liu <kliu7799@gmail.com>
2021-11-15 22:43:16 -0500

    test

--- nonstop_networking/common.c
+++ nonstop_networking/common.c
@@ -1,49 +1,59 @@
-/**
- * nonstop_networking
- * CS 241 - Fall 2021
- */
 #include "common.h"
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/epoll.h>
-#include "format.h"
-#include <stdio.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <errno.h>
-#include <netdb.h>
-#include <signal.h>
+#include <stdio.h>
 
-
-
-ssize_t read_from_socket(int socket, char *buffer, size_t count) {
+ssize_t read_all_from_socket(int socket, char *buffer, size_t count) {
     // Your Code Here
-
-    size_t result = 0;
-    while (result < count) {
-      ssize_t read = read(socket, (void*)(buffer + result), count - result);
-      if (read == -1 && errno == EINTR) continue;
-      if (read == 0) break;
-      if (read == -1) return -1;
-      result += read;
+    size_t total_amount_read = 0;
+    while (total_amount_read < count) {
+        ssize_t bytes_read = read(socket, buffer + total_amount_read, count - total_amount_read);
+        if (bytes_read == 0) {
+            break;
+        } else if (bytes_read > 0) {
+            total_amount_read += bytes_read;
+        } else if (bytes_read == -1 && errno == EINTR) {
+            continue;
+        } else {
+            return -1;
+        }
     }
-    return result;
+    return total_amount_read;
 }
 
-ssize_t write_to_socket(int socket, const char *buffer, size_t count) {
-    size_t result = 0;
-    while (result < count) {
-      ssize_t write = write(socket, (void*)(buffer + result), count - result);
-      if (write == -1 && errno == EINTR) continue;
-      if (write == 0) break;
-      if (write == -1) return -1;
-      result += write;
+ssize_t write_all_to_socket(int socket, const char *buffer, size_t count) {
+    // Your Code Here
+    size_t total_amount_written = 0;
+    while (total_amount_written < count) {
+        ssize_t bytes_written = write(socket, buffer + total_amount_written, count - total_amount_written);
+        if (bytes_written == 0) {
+            break;
+        } else if (bytes_written > 0) {
+            total_amount_written += bytes_written;
+        } else if (bytes_written == -1 && errno == EINTR) {
+            continue;
+        } else {
+            return -1;
+        }
     }
-    return result;
+    return total_amount_written;
+}
+
+int write_localfile_to_socket(int socket, FILE* local_file, size_t file_size) {
+    size_t total_bytes_written = 0;
+    size_t blocksize = 512;
+    char buf[blocksize];
+    //write blocksize of bytes each time
+    while (total_bytes_written < file_size) {
+        if (file_size - total_bytes_written < blocksize) {
+            blocksize = file_size - total_bytes_written;
+        }
+        size_t bytes_read = fread(buf, sizeof(char), blocksize, local_file);
+        if (bytes_read == 0) break;
+        if (write_all_to_socket(socket, buf, bytes_read) == -1) {
+            return -1;
+        }
+        total_bytes_written += bytes_read;
+    }
+    return 0;
 }
--- nonstop_networking/common.h
+++ nonstop_networking/common.h
@@ -15,5 +15,6 @@
 typedef enum { GET, PUT, DELETE, LIST, V_UNKNOWN } verb;
 
 
-ssize_t read_from_socket(int socket, char *buffer, size_t count);
-ssize_t write_to_socket(int socket, const char *buffer, size_t count);
+ssize_t read_all_from_socket(int socket, char *buffer, size_t count);
+ssize_t write_all_to_socket(int socket, const char *buffer, size_t count);
+int write_localfile_to_socket(int socket, FILE* local_file, size_t file_size);
