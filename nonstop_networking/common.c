2a59b93f53d3104ace11e81e8b7014c589d64f6e
Kimmy Liu <kliu7799@gmail.com>
2021-11-15 22:51:01 -0500

    test

--- nonstop_networking/common.c
+++ nonstop_networking/common.c
@@ -1,9 +1,12 @@
 /**
  * nonstop_networking
- * CS 241 - Spring 2021
+ * CS 241 - Fall 2021
  */
 #include "common.h"
-#include "common.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
 #include "format.h"
 #include <stdio.h>
 #include <stdio.h>
@@ -16,38 +19,31 @@
 #include <errno.h>
 #include <netdb.h>
 #include <signal.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/epoll.h>
+
+
 
 ssize_t read_from_socket(int socket, char *buffer, size_t count) {
     // Your Code Here
-    size_t r_count = 0;
-    while (r_count < count) {
-      ssize_t ret = read(socket, buffer + r_count, count - r_count);
-      if (ret == 0) break;
-      if (ret == -1 && errno == EINTR) continue;
-      if (ret == -1) {
-        perror("read_from_socket");
-        return -1;
-      }
-      r_count += ret;
+
+    size_t result = 0;
+    while (result < count) {
+      ssize_t read = read(socket, buffer + result, count - result);
+      if (read == -1 && errno == EINTR) continue;
+      if (read == 0) break;
+      if (read == -1) return -1;
+      result += read;
     }
-    return r_count;
+    return result;
 }
 
 ssize_t write_to_socket(int socket, const char *buffer, size_t count) {
-    size_t w_count = 0;
-    while (w_count < count) {
-      ssize_t ret = write(socket, buffer + w_count, count - w_count);
-      if (ret == 0) break;
-      if (ret == -1 && errno == EINTR) continue;
-      if (ret == -1) {
-        perror("write_from_socket");
-        return -1;
-      }
-      w_count += ret;
+    size_t result = 0;
+    while (result < count) {
+      ssize_t write = write(socket, buffer + result, count - result);
+      if (write == -1 && errno == EINTR) continue;
+      if (write == 0) break;
+      if (write == -1) return -1;
+      result += write;
     }
-    return w_count;
+    return result;
 }
