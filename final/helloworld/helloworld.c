d4a421b49e0631b56ee63d59c20a87f8042e873a
Kimmy Liu <zl32@illinois.edu>
2021-12-15 22:30:33 -0500

    hello

--- final/helloworld/helloworld.c
+++ final/helloworld/helloworld.c
@@ -4,105 +4,91 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/mman.h>
-#include <fcntl.h>
 
-int check_file_exists(char* filename) {
+
+int exist_file(char* name) {
     struct stat s;
-    if (stat(filename, &s) == 0) {
+    if (stat(name, &s) == 0) {
         return 1;
     }
     return 0;
 }
 
-off_t get_file_size(char* filename) {
+off_t get_file_size(char* name) {
     struct stat s;
-    if (stat(filename, &s) == 0) {
+    if (stat(name, &s) == 0) {
         return s.st_size;
     }
-    // error
     return -1;
 }
 
 int main(int argc, char* argv[]) {
     if (argc >= 3 && strcmp(argv[0], "./encrypt") == 0) {
-        // encrypt mode
 
         char* input = argv[1];
-        char* output1 = argv[2];
+        char* output = argv[2];
 
-        if (check_file_exists(input) == 0) {
-            // input file does not exist
+        if (exist_file(input) == 0) {
             printf("Hello World\n");
             exit(0);
         } 
 
-        // open input file with open and mmap
-        int input_fd = open(input, O_RDWR);
-        off_t input_size = get_file_size(input);
-        if (input_fd == -1) {
-            // error when opening input file
+        int infd = open(input, O_RDWR);
+        off_t getfSize = get_file_size(input);
+        if (infd == -1) {
             exit(1);
         }
-        char* input_buffer = mmap(NULL, input_size, PROT_READ | PROT_WRITE, MAP_SHARED, input_fd, 0);
+        char* inbuff = mmap(NULL, getfSize, PROT_READ | PROT_WRITE, MAP_SHARED, infd, 0);
 
-        // create output files
-        FILE* fp_out1 = fopen(output1, "w");
-        if (fp_out1 == NULL) {
-            // error when creating output files
+        FILE* file1 = fopen(output, "w");
+        if (file1 == NULL) {
             exit(1);
         }
 
-        // get random bytes from /dev/urandom
-        int random_fd = open("/dev/urandom", O_RDONLY);
-        char random_buffer[input_size];
-        if (read(random_fd, random_buffer, input_size) == -1) {
-            // error when reading random bytes
+        int rfd = open("/dev/urandom", O_RDONLY);
+        char rbuff[getfSize];
+        if (read(rfd, rbuff, getfSize) == -1) {
             exit(1);
         }
 
-        // modify output and input files
-        int i = 0;
-        for(i = 0; i < input_size; i++) {
-            fputc(random_buffer[i], fp_out1);
+        for(int i = 0; i < getfSize; i++) {
+            fputc(rbuff[i], file1);
         }
 
-        for(i = 0; i < input_size; i++) {
-            fputc(random_buffer[i] ^ input_buffer[i], fp_out1);
-            input_buffer[i] = 0xff;
+        for(int i = 0; i < getfSize; i++) {
+            fputc(rbuff[i] ^ inbuff[i], file1);
+            inbuff[i] = 0xff;
         }
 
-        // clean up 
-        munmap(input_buffer, input_size);
-        close(input_fd);
-        fclose(fp_out1);
+        munmap(inbuff, getfSize);
+        close(infd);
+        fclose(file1);
 
     } else if (argc >= 3 && strcmp(argv[0], "./decrypt") == 0) {
         // decrypt mode
 
-        char* input1 = argv[1];
-
-        // process input files using open and mmap
-        int input_fd1 = open(input1, O_RDONLY);
-        off_t input_file_size = get_file_size(input1);
-
-        char* input_buffer1 = mmap(NULL, input_file_size, PROT_READ, MAP_SHARED, input_fd1, 0);
+        char* input = argv[1];
+        int infd1 = open(input, O_RDONLY);
+        off_t getfSize = get_file_size(input);
+        char* inbuff = mmap(NULL, getfSize, PROT_READ, MAP_SHARED, infd1, 0);
 
         // create and write output file
         char* output = argv[2];
-        FILE* fp_out = fopen(output, "w");
+        FILE* outfile = fopen(output, "w");
 
-        int len = input_file_size / 2;
+        int len = getfSize / 2;
         for (int i = 0; i < len; ++i) {
-            fputc(input_buffer1[i] ^ input_buffer1[i+len], fp_out);
+            fputc(inbuff[i] ^ inbuff[i+len], outfile);
         }
-        // clean up and exit
-        close(input_fd1);
-        fclose(fp_out);
-        unlink(input1);
+
+        close(infd1);
+        fclose(outfile);
+        unlink(input);
     } else {
         printf("Hello World\n");
         exit(0);
